688ab59d7bde7334c600f65a3ee9f8bbe681c55e
DanVi007 <danvitrinh@outlook.com>
2024-01-15 17:33:46 +0100

    terminal keymaps and faster movement in visual mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- escape
@@ -10,6 +10,16 @@ keymap("i", "jk", "<ESC>", opts)
 -- faster movement
 keymap("n", "J", "5j", opts)
 keymap("n", "K", "5k", opts)
-
+keymap("v", "J", "5j", opts) -- faster movement
+keymap("v", "K", "5k", opts) -- faster movement
 -- join lines
-keymap("n", "<leader>j", "J", opts)
+keymap("n", "<leader>j", "J", { noremap = true, silent = true, desc = "Join lines" })
+
+-- to move in terminal
+keymap("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+keymap("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+keymap("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+keymap("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+
+-- to escape terminal
+keymap("t", "jk", [[<C-\><C-n>]], opts)
