fc84bf05a7f4c2b1be141754cc8c762a0cb41f69
DanVi007 <danvitrinh@outlook.com>
2024-01-27 23:51:41 +0100

    removed terminal specify keymaps since moving to tmux

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,18 +15,6 @@ keymap("v", "K", "5k", opts) -- faster movement
 -- join lines
 keymap("n", "<leader>j", "J", { noremap = true, silent = true, desc = "Join lines" })
 
--- to move in terminal
-keymap("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-keymap("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-keymap("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-keymap("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-
--- to escape terminal
--- disabled because it messes with moving in lazygit
--- keymap("t", "jk", [[<C-\><C-n>]], opts)
--- replaced with normal escape
-keymap("t", "<Esc>", [[<C-\><C-n>]], opts)
-
 -- copy to system clip board
 keymap({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to system clipboard like y" })
 keymap("n", "<leader>Y", [["+Y]], { desc = "Copy to system clipboard like Y" })
