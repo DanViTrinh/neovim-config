ab96ca92be13b76ef29d5fdc958d1b9054bdd60d
DanVi007 <danvitrinh@outlook.com>
2024-01-27 21:31:30 +0100

    update and unelegant way of disabling copilot on startup

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- not a very elegant solution since copilot is loaded even though it is not used
+-- https://www.reddit.com/r/neovim/comments/13td06h/copilotlua_is_there_a_way_to_start_with_copilot/
+vim.cmd(":Copilot disable")
