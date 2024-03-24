9db9d7fd55dd9d3bc00219062964345ad95fec9f
DanViTrinh <danvitrinh@outlook.com>
2024-03-24 19:51:02 +0100

    autocmd for filetypes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- specify kmonad files to lisp
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*.kbd",
+  command = "set filetype=lisp",
+})
+
+-- for conf files
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*.conf",
+  command = "set filetype=conf",
+})
